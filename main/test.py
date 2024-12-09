2c411e08c004cb1afe3ce8b1d0b1428e8981c806
Dimatron74 <logachev_16@bk.ru>
2024-12-09 13:18:00 +0300

    remove test autotranslators

--- main/test.py
+++
@@ -1,4 +0,0 @@
-from deep_translator import YandexTranslator
-
-translated = YandexTranslator('b1g394q49o6nsje338sk').translate(source="auto", target="en", text='Hallo, Welt')
-print(f"translated text: {translated}")
